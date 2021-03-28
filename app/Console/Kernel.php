14f4246a3111347d152336393e24141b1e1e3a10
KMP <krzysztof.picur@gmail.com>
2021-03-28 18:45:27 +0200

    added cron package

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
+        'App\Console\Commands\ChangeNoticeStatusCommand',
         //
     ];
 
@@ -24,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('change:status')->everyMinute();
     }
 
     /**
