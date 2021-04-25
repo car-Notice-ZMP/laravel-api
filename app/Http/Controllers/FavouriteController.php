6fc8f15485cea0fb356848592bd2808046b06570
KMP <krzysztof.picur@gmail.com>
2021-04-25 21:37:47 +0200

    return favourite notices with statuses

--- app/Http/Controllers/FavouriteController.php
+++ app/Http/Controllers/FavouriteController.php
@@ -6,48 +6,39 @@ use App\Models\Notice;
 
 class FavouriteController extends Controller
 {
-    //
-    public function store ($id)
+    public function store($id)
     {
-
         $user   = auth()->user();
-
         $notice = Notice::find($id);
 
         $user->favorite($notice);
 
-        return response()->json(['message' => 'Dodano do ulubionych']);
-
+        return response()->json(['message' => 'Dodano do ulubionych'], 200, [],JSON_UNESCAPED_SLASHES);
     }
 
-    public function show ()
+    public function show()
     {
         $user   = auth()->user();
+        $notice = $user->getFavoriteItems(Notice::class)->with('statuses');
 
-        $notice = $user->getFavoriteItems(Notice::class);
-
-        return response()->json(['favourites' => $notice->get()]);
+        return response()->json(['favourites' => $notice->get()], 200, [],JSON_UNESCAPED_SLASHES);
     }
 
-    public function unfavourite ($id)
+    public function unfavourite($id)
     {
         $user   = auth()->user();
-
         $notice = Notice::find($id);
 
         $user->unfavorite($notice);
 
         return response()->json(['message' => 'Usunięto z ulubionych']);
-
     }
 
-    public function count ()
+    public function count()
     {
         $user   = auth()->user();
-
         $total = $user->favorites()->count();
 
         return response()->json(['Ilość polubień' => $total]);
-
     }
 }
