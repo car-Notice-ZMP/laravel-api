ce77c2a084078c92124292ab92ef603d57fbcebf
KMP <krzysztof.picur@gmail.com>
2021-05-04 19:03:20 +0200

    some changes in users_table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable();
+            $table->string('name');
             $table->string('email')->nullable();
             $table->integer('admin')->default(0);
             $table->timestamp('email_verified_at')->nullable();
